bbcab8a7f9c7cb4b3b3b3b6ec280ca3bf44e3d47
evantahler <evan@airbyte.io>
2023-05-15 15:57:19 -0600

    increase efficiency of typying and deduplication by removing full table scans

--- one-table.postgres.sql
+++ one-table.postgres.sql
@@ -123,9 +123,9 @@ CREATE TABLE IF NOT EXISTS z_airbyte.users_raw (
     "_airbyte_read_at" timestamp NOT NULL, -- Airbyte column, cannot be null
     "_airbyte_typed_at" timestamp -- Airbyte column
 );
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON public.users USING BTREE ("_airbyte_raw_id");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON public.users USING BTREE ("_airbyte_read_at");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON public.users USING BTREE ("_airbyte_typed_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON z_airbyte.users_raw USING BTREE ("_airbyte_raw_id");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_read_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_typed_at");
 
 -- Step 1: Load the raw data
 
@@ -137,8 +137,38 @@ INSERT INTO z_airbyte.users_raw ("_airbyte_data", "_airbyte_raw_id", "_airbyte_r
 -- Moving the data and deduping happens in a transaction to prevent duplicates from appearing
 BEGIN;
 
--- Step 2: Type the Data & handle errors
+
+-- Step 2: First, delete any old entries from the raw table which have new records
+-- This might be better than using row_number() after inserting the new data into the raw table because the set of PKs to consider will likely be smaller.  The trade is a second round of SAFE_CAST. if that's fast, it might be a good idea
+
+DELETE FROM z_airbyte.users_raw
+WHERE _airbyte_raw_id IN (
+	SELECT _airbyte_raw_id
+	FROM public.users
+	WHERE
+		id IN (
+			SELECT _airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+			FROM z_airbyte.users_raw
+			WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+		)
+)
+;
+
+-- Step 3: Also, delete any old entries from the typed table which have new records
+
+DELETE FROM public.users
+WHERE id in (
+	SELECT
+		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+	FROM z_airbyte.users_raw
+	WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+)
+;
+
+
+-- Step 3: Type the Data & handle errors
 -- Note: We know the column names from the schema, so we don't need to anything refelxive to look up the column names
+-- Don't insert rows which have been deleted by CDC
 
 INSERT INTO public.users
 SELECT
@@ -157,41 +187,11 @@ SELECT
 	_airbyte_read_at
 FROM z_airbyte.users_raw
 WHERE
-	_airbyte_typed_at IS NULL -- inserting only null values, we can recover from failed previous checkpoints
+	_airbyte_typed_at IS NULL -- inserting only new/null values, we can recover from failed previous checkpoints
+	AND _airbyte_data ->> '_ab_cdc_deleted_at' IS NULL -- Skip CDC deleted rows (old records are already cleared away above
 ;
 
--- Step 3: De-dupe Typed Table
--- ... assuming the user wanted dedupe
--- NOTE: Postgres doesn't "need" _airbyte_raw_id and could use ctid, but to match cloud DWs, that's what we use in this example
-WITH cte AS (
-	SELECT _airbyte_raw_id, row_number() OVER (
-		PARTITION BY id ORDER BY _airbyte_read_at DESC
-	) as row_number FROM public.users
-)
-
-DELETE FROM public.users
-WHERE _airbyte_raw_id in (
-	SELECT _airbyte_raw_id FROM cte WHERE row_number != 1
-)
-;
-
--- Step 4: Deal with CDC Deletion (and other special cases)
-DELETE FROM public.users WHERE id IN (
-	SELECT
-		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id -- based on the PK which we know from the connector catalog
-	FROM z_airbyte.users_raw
-	WHERE _airbyte_data ->> '_ab_cdc_deleted_at' IS NOT NULL
-)
-;
-
--- Step 5: Remove old entries from Raw table
-DELETE FROM z_airbyte.users_raw
-WHERE _airbyte_raw_id NOT IN (
-	SELECT _airbyte_raw_id FROM public.users
-)
-;
-
--- Step 6: Apply typed_at timestamp where needed
+-- Step 4: Apply typed_at timestamp where needed
 UPDATE z_airbyte.users_raw
 SET _airbyte_typed_at = NOW()
 WHERE _airbyte_typed_at IS NULL
@@ -213,9 +213,9 @@ CREATE TABLE IF NOT EXISTS z_airbyte.users_raw (
     "_airbyte_read_at" timestamp NOT NULL, -- Airbyte column, cannot be null
     "_airbyte_typed_at" timestamp -- Airbyte column
 );
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON public.users USING BTREE ("_airbyte_raw_id");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON public.users USING BTREE ("_airbyte_read_at");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON public.users USING BTREE ("_airbyte_typed_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON z_airbyte.users_raw USING BTREE ("_airbyte_raw_id");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_read_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_typed_at");
 
 -- Step 1: Load the raw data
 -- No update for Evan (user 1)
@@ -229,8 +229,38 @@ INSERT INTO z_airbyte.users_raw ("_airbyte_data", "_airbyte_raw_id", "_airbyte_r
 -- Moving the data and deduping happens in a transaction to prevent duplicates from appearing
 BEGIN;
 
--- Step 2: Type the Data & handle errors
+
+-- Step 2: First, delete any old entries from the raw table which have new records
+-- This might be better than using row_number() after inserting the new data into the raw table because the set of PKs to consider will likely be smaller.  The trade is a second round of SAFE_CAST. if that's fast, it might be a good idea
+
+DELETE FROM z_airbyte.users_raw
+WHERE _airbyte_raw_id IN (
+	SELECT _airbyte_raw_id
+	FROM public.users
+	WHERE
+		id IN (
+			SELECT _airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+			FROM z_airbyte.users_raw
+			WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+		)
+)
+;
+
+-- Step 3: Also, delete any old entries from the typed table which have new records
+
+DELETE FROM public.users
+WHERE id in (
+	SELECT
+		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+	FROM z_airbyte.users_raw
+	WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+)
+;
+
+
+-- Step 3: Type the Data & handle errors
 -- Note: We know the column names from the schema, so we don't need to anything refelxive to look up the column names
+-- Don't insert rows which have been deleted by CDC
 
 INSERT INTO public.users
 SELECT
@@ -249,41 +279,11 @@ SELECT
 	_airbyte_read_at
 FROM z_airbyte.users_raw
 WHERE
-	_airbyte_typed_at IS NULL -- inserting only null values, we can recover from failed previous checkpoints
+	_airbyte_typed_at IS NULL -- inserting only new/null values, we can recover from failed previous checkpoints
+	AND _airbyte_data ->> '_ab_cdc_deleted_at' IS NULL -- Skip CDC deleted rows (old records are already cleared away above
 ;
 
--- Step 3: De-dupe Typed Table
--- ... assuming the user wanted dedupe
--- NOTE: Postgres doesn't "need" _airbyte_raw_id and could use ctid, but to match cloud DWs, that's what we use in this example
-WITH cte AS (
-	SELECT _airbyte_raw_id, row_number() OVER (
-		PARTITION BY id ORDER BY _airbyte_read_at DESC
-	) as row_number FROM public.users
-)
-
-DELETE FROM public.users
-WHERE _airbyte_raw_id in (
-	SELECT _airbyte_raw_id FROM cte WHERE row_number != 1
-)
-;
-
--- Step 4: Deal with CDC Deletion (and other special cases)
-DELETE FROM public.users WHERE id IN (
-	SELECT
-		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id -- based on the PK which we know from the connector catalog
-	FROM z_airbyte.users_raw
-	WHERE _airbyte_data ->> '_ab_cdc_deleted_at' IS NOT NULL
-)
-;
-
--- Step 5: Remove old entries from Raw table
-DELETE FROM z_airbyte.users_raw
-WHERE _airbyte_raw_id NOT IN (
-	SELECT _airbyte_raw_id FROM public.users
-)
-;
-
--- Step 6: Apply typed_at timestamp where needed
+-- Step 4: Apply typed_at timestamp where needed
 UPDATE z_airbyte.users_raw
 SET _airbyte_typed_at = NOW()
 WHERE _airbyte_typed_at IS NULL
@@ -305,9 +305,9 @@ CREATE TABLE IF NOT EXISTS z_airbyte.users_raw (
     "_airbyte_read_at" timestamp NOT NULL, -- Airbyte column, cannot be null
     "_airbyte_typed_at" timestamp -- Airbyte column
 );
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON public.users USING BTREE ("_airbyte_raw_id");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON public.users USING BTREE ("_airbyte_read_at");
-CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON public.users USING BTREE ("_airbyte_typed_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_raw_id" ON z_airbyte.users_raw USING BTREE ("_airbyte_raw_id");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_read_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_read_at");
+CREATE INDEX IF NOT EXISTS "idx_users_raw__airbyte_typed_at" ON z_airbyte.users_raw USING BTREE ("_airbyte_typed_at");
 
 -- Step 1: Load the raw data
 -- Delete row 1 with CDC
@@ -317,8 +317,38 @@ INSERT INTO z_airbyte.users_raw ("_airbyte_data", "_airbyte_raw_id", "_airbyte_r
 -- Moving the data and deduping happens in a transaction to prevent duplicates from appearing
 BEGIN;
 
--- Step 2: Type the Data & handle errors
+
+-- Step 2: First, delete any old entries from the raw table which have new records
+-- This might be better than using row_number() after inserting the new data into the raw table because the set of PKs to consider will likely be smaller.  The trade is a second round of SAFE_CAST. if that's fast, it might be a good idea
+
+DELETE FROM z_airbyte.users_raw
+WHERE _airbyte_raw_id IN (
+	SELECT _airbyte_raw_id
+	FROM public.users
+	WHERE
+		id IN (
+			SELECT _airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+			FROM z_airbyte.users_raw
+			WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+		)
+)
+;
+
+-- Step 3: Also, delete any old entries from the typed table which have new records
+
+DELETE FROM public.users
+WHERE id in (
+	SELECT
+		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id
+	FROM z_airbyte.users_raw
+	WHERE _airbyte_typed_at IS NULL -- considering only new/null values, we can recover from failed previous checkpoints
+)
+;
+
+
+-- Step 3: Type the Data & handle errors
 -- Note: We know the column names from the schema, so we don't need to anything refelxive to look up the column names
+-- Don't insert rows which have been deleted by CDC
 
 INSERT INTO public.users
 SELECT
@@ -337,41 +367,11 @@ SELECT
 	_airbyte_read_at
 FROM z_airbyte.users_raw
 WHERE
-	_airbyte_typed_at IS NULL -- inserting only null values, we can recover from failed previous checkpoints
+	_airbyte_typed_at IS NULL -- inserting only new/null values, we can recover from failed previous checkpoints
+	AND _airbyte_data ->> '_ab_cdc_deleted_at' IS NULL -- Skip CDC deleted rows (old records are already cleared away above
 ;
 
--- Step 3: De-dupe Typed Table
--- ... assuming the user wanted dedupe
--- NOTE: Postgres doesn't "need" _airbyte_raw_id and could use ctid, but to match cloud DWs, that's what we use in this example
-WITH cte AS (
-	SELECT _airbyte_raw_id, row_number() OVER (
-		PARTITION BY id ORDER BY _airbyte_read_at DESC
-	) as row_number FROM public.users
-)
-
-DELETE FROM public.users
-WHERE _airbyte_raw_id in (
-	SELECT _airbyte_raw_id FROM cte WHERE row_number != 1
-)
-;
-
--- Step 4: Deal with CDC Deletion (and other special cases)
-DELETE FROM public.users WHERE id IN (
-	SELECT
-		_airbyte_safe_cast_to_integer(_airbyte_data ->> 'id') as id -- based on the PK which we know from the connector catalog
-	FROM z_airbyte.users_raw
-	WHERE _airbyte_data ->> '_ab_cdc_deleted_at' IS NOT NULL
-)
-;
-
--- Step 5: Remove old entries from Raw table
-DELETE FROM z_airbyte.users_raw
-WHERE _airbyte_raw_id NOT IN (
-	SELECT _airbyte_raw_id FROM public.users
-)
-;
-
--- Step 6: Apply typed_at timestamp where needed
+-- Step 4: Apply typed_at timestamp where needed
 UPDATE z_airbyte.users_raw
 SET _airbyte_typed_at = NOW()
 WHERE _airbyte_typed_at IS NULL
