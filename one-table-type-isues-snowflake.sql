bfd7268754c58faffcc64754ef6d4ec7ef05e0a6
Evan Tahler <evan@airbyte.io>
2023-06-08 15:09:05 -0700

    More extreme type checks in snowflake (#11)
    * More extreme type checks in snowflake
    
    * add schema
    
    * all schema tables other than PK are nullable

--- one-table-type-isues-snowflake.sql
+++ one-table-type-isues-snowflake.sql
@@ -13,15 +13,8 @@ Schema:
   }],
 	"updated_at": timestamp
 }
-
-KNOWN LIMITATIONS
-* Only one error type shown per row, the first one sequentially
-* There's a full table scan used for de-duplication.  This can be made more efficient...
-* It would be better to show the actual error message from the DB, not custom "this column is bad" strings
 */
 
--- Set up the Experiment
--- Assumption: We can build the table at the start of the sync based only on the schema we get from the source/configured catalog
 
 DROP TABLE IF EXISTS PUBLIC.USERS;
 DROP TABLE IF EXISTS Z_AIRBYTE.USERS_RAW;
@@ -134,158 +127,94 @@ BEGIN
 END
 $$;
 
-----------------------------
---------- SYNC 1 -----------
-----------------------------
+---------------------
+-- BAD DATA CHECKS --
+---------------------
 
 CALL PUBLIC._AIRBYTE_PREPARE_RAW_TABLE();
 
--- Load the raw data
+/*
+Problem: Writing a decimal to an int column
+Outcome: Snowflake truncates the decimal to an int, no error
+*/
 
 INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 1,
-	first_name: "Evan",
-	age: 38,
-	updated_at: "2020-01-01T00:00:00Z",
-	address:{
-		city: "San Francisco",
-		zip: "94001"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 2,
-	first_name: "Brian",
-	age: 39,
-	updated_at: "2020-01-01T00:00:01Z",
-	address:{
-		city: "Menlo Park",
-		zip: "94002"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 3,
-	first_name: "Edward",
-	age: 40,
-	updated_at: "2020-01-01T00:00:02Z",
-	address:{
-		city: "Sunyvale",
-		zip: "94003"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
--- Joe is missing an age, null OK
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 4,
-	first_name: "Joe",
-	age: 40,
-	updated_at: "2020-01-01T00:00:03Z",
-	address:{
-		city: "Seattle",
-		zip: "98999"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
-
-CALL PUBLIC._AIRBYTE_TYPE_DEDUPE();
-
-----------------------------
---------- SYNC 2 -----------
-----------------------------
-
-CALL PUBLIC._AIRBYTE_PREPARE_RAW_TABLE();
-
--- Load the raw data
--- Age update for Evan (user 1)
--- There is an update for Brian (user 2, new address.zip)
--- There is an update for Edward (user 3, age is invalid)
--- No update for Joe (user 4)
-
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 1,
-	first_name: "Evan",
-	age: 39,
+	id: 101,
+	first_name: "bad-decimal-number-to-int",
+	age: 0.1,
 	updated_at: "2020-01-02T00:00:00Z",
 	address:{
-		city: "San Francisco",
-		zip: "94001"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 2,
-	first_name: "Brian",
-	age: 39,
-	updated_at: "2020-01-02T00:00:01Z",
-	address:{
-		city: "Menlo Park",
-		zip: "99999"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
-INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 3,
-	first_name: "Edward",
-	age: "forty",
-	updated_at: "2020-01-02T00:00:02Z",
-	address:{
-		city: "Sunyvale",
-		zip: "94003"
+		city: "city",
+		zip: "01234"
 } }$$), UUID_STRING(), CURRENT_TIMESTAMP();
 
-CALL PUBLIC._AIRBYTE_TYPE_DEDUPE();
-
-----------------------------
---------- SYNC 3 -----------
-----------------------------
-
-CALL PUBLIC._AIRBYTE_PREPARE_RAW_TABLE();
-
--- Step 1: Load the raw data
--- Delete row 1 with CDC
+/*
+Problem: Writing a negative number to an unsigned int column
+Outcome: Snowflake doesn't make a distinction about signed vs unsigned, no error
+*/
 
 INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	_ab_cdc_deleted_at: true,
-	id: 2,
-	first_name: "Brian",
-	age: 39,
-	updated_at: "2020-01-03T00:00:00Z",
+	id: 102,
+	first_name: "bad-negative-int-to-int",
+	age: -1,
+	updated_at: "2020-01-02T00:00:00Z",
 	address:{
-		city: "Menlo Park",
-		zip: "99999"
+		city: "city",
+		zip: "01234"
 } }$$), UUID_STRING(), CURRENT_TIMESTAMP();
+
+/*
+Problem: Writing a number lager than snowflake can handle to an int column
+Outcome: NULL, typing error caught
+Learn more: (99999999999999999999999999999999999999 + 1) https://docs.snowflake.com/en/sql-reference/data-types-numeric
+*/
+
 INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 5,
-	first_name: "Cynthia",
-	age: 40,
-	updated_at: "2020-01-03T00:00:01Z",
+	id: 103,
+	first_name: "bad-int-too-big",
+	age: 9999999999999999999999999999999999999900000,
+	updated_at: "2020-01-02T00:00:00Z",
 	address:{
-		city: "Redwood City",
-		zip: "98765"
+		city: "city",
+		zip: "01234"
 } }$$), UUID_STRING(), CURRENT_TIMESTAMP();
+
+/*
+Problem: Writing a negative (B.C.E.) date value
+Outcome: NULL, typing error caught
+*/
+
 INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 5,
-	first_name: "Cynthia",
-	age: 41,
-	updated_at: "2020-01-03T00:00:02Z",
+	id: 104,
+	first_name: "bad-negative-date",
+	age: 123,
+	updated_at: "-2020-01-02T00:00:00Z",
 	address:{
-		city: "Redwood City",
-		zip: "98765"
+		city: "city",
+		zip: "01234"
 } }$$), UUID_STRING(), CURRENT_TIMESTAMP();
+
+/*
+Problem: Writing bas JSON to a JSON column
+Outcome: 'foo', there isn't a "safe_cast" for JSON in snowflake, so it's fine.  We might want to validate this manually somehow.
+*/
+
 INSERT INTO Z_AIRBYTE.USERS_RAW ("_airbyte_data", "_airbyte_raw_id", "_airbyte_extracted_at") SELECT PARSE_JSON($${
-	id: 5,
-	first_name: "Cynthia",
-	age: 42,
-	updated_at: "2020-01-03T00:00:03Z",
-	address:{
-		city: "Redwood City",
-		zip: "98765"
-} }$$), UUID_STRING(), CURRENT_TIMESTAMP();
+	id: 105,
+	first_name: "bad-sub-json",
+	age: 123,
+	updated_at: "2020-01-02T00:00:00Z",
+	address:'foo'
+}$$), UUID_STRING(), CURRENT_TIMESTAMP();
 
 CALL PUBLIC._AIRBYTE_TYPE_DEDUPE();
 
 ----------------------
 -- FINAL VALIDATION --
 ----------------------
+
 /*
-
-You should see 5 RAW records, one for each of the 5 users
-You should see 4 TYPED records, one for each user, except user #2, which was CDC deleted
-You should have the latest data for each user in the typed final table:
-  * User #1 (Evan) has the latest data (age=39)
-  * User #3 (Edward) has a null age [+ error] due to that age being un-typable
-  * User #4 (Joe) has a null age & no errors
-  * User #5 (Cynthia) has one entry dispite the multiple insertes, with the latest entry (age=42)
-
+There should be no errors.  All rows above should have made it to the final table
 */
 
 SELECT CURRENT_TIMESTAMP();
